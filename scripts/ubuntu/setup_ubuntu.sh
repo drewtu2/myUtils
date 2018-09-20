c06e6a5288f5e72d07e71f5daca9a96b4ed68ca3
Andrew Tu <drewtu2@yahoo.com>
2018-09-20 16:47:17 -0700

    updates

--- scripts/ubuntu/setup_ubuntu.sh
+++ scripts/ubuntu/setup_ubuntu.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 # Just update the OS related stuff.
-sudo apt-get dist-upgrade
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -qq dist-upgrade
+sudo apt-get -qq update
+sudo apt-get -qq upgrade
 
 # Get some useful packages
-sudo apt-get install vim
-sudo apt-get install tmux
-sudo apt-get install python3
-sudo apt-get install python3-venv
+sudo apt-get install -qq vim
+sudo apt-get install -qq tmux
+
+# Install and update python
+sudo apt-get install -qq python3
+sudo apt-get install -qq python3-venv
+pip install --upgrade pip
+pip3 install --upgrade pip
 
 # Install git and then clone the utils repo
 # If this script is running, this has already been done...
 #sudo apt-get install git
 #git clone https://www.github.com/drewtu2/myUtils.git
 
-
+# Usefull for gnome-open
+# gnome-open will open files with the appropriate software
+sudo apt-get install -qq libgnome2-bin
